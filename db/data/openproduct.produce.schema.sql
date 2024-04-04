8700829f94c34ebe4d4e87364c5698af3d84ce5f
alberic <alberic.briot@osiris-team.fr>
2024-04-04 16:10:52 +0200

    Backup before get gogocarto datas

--- db/data/openproduct.produce.schema.sql
+++ db/data/openproduct.produce.schema.sql
@@ -31,7 +31,7 @@ CREATE TABLE `produce` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `produce_unique` (`fr`),
   UNIQUE KEY `produce_unique_1` (`en`)
-) ENGINE=InnoDB AUTO_INCREMENT=234 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=273 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -43,4 +43,4 @@ CREATE TABLE `produce` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-03-12  0:30:52
+-- Dump completed on 2024-04-04 16:10:14
